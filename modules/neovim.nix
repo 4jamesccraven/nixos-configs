faa5aa3925d6d149890693d0e2cd4d02b15e58a2
4jamesccraven <4jamesccraven@gmail.com>
2024-06-18 14:54:41 -0400

    aliased nvim to vi and vim, added color schemes as nvim 0.10 seems to have removed colour schemes...

--- modules/neovim.nix
+++ modules/neovim.nix
@@ -5,13 +5,20 @@
   home-manager.users.jamescraven = {
     programs.neovim = {
       enable = true;
+      viAlias = true;
+      vimAlias = true;
 
       defaultEditor = true;
 
       # Plugins
       plugins = with pkgs.vimPlugins; [
         telescope-nvim
+        catppuccin-nvim
       ];
+
+      extraConfig = ''
+        colorscheme catppuccin-frappe
+      '';
 
     };
   };
