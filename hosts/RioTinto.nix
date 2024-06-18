fa28173bdbfc991ab5be7e29b18e8b1801498cf8
4jamesccraven <4jamesccraven@gmail.com>
2024-06-18 03:07:04 -0400

    Add rgb support

--- hosts/RioTinto.nix
+++ hosts/RioTinto.nix
@@ -3,7 +3,38 @@
 {
   imports = [
     ./common.nix
-  ]
+  ];
 
   networking.hostName = "RioTinto";
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  ## Nvidia ##
+
+  services.xserver.videoDrivers = ["nvidia"];
+
+  hardware = {
+    opengl = {
+      enable = true;
+      driSupport = true;
+    };
+
+
+    nvidia = {
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
+      modesetting.enable = true;
+      powerManagement = {
+        enable = false;
+        finegrained = false;
+      };
+      
+      open = false;
+      nvidiaSettings = true;
+    };
+
+  };
+
+  services.hardware.openrgb.enable = true;
+  
 }
