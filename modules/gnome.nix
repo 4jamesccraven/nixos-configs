bd12af94ca9078b3b7552637d006b332f56c7c94
4jamesccraven <4jamesccraven@gmail.com>
2024-06-30 02:10:32 -0400

    Removed xterm

--- modules/gnome.nix
+++ modules/gnome.nix
@@ -10,8 +10,14 @@
     services.xserver.enable = true;
 
     # Enable GNOME
-    services.xserver.displayManager.gdm.enable = true;
-    services.xserver.desktopManager.gnome.enable = true;
+    services.xserver = {
+      displayManager.gdm.enable = true;
+      desktopManager.gnome.enable = true;
+
+      excludePackages = with pkgs; [
+        xterm
+      ];
+    };
 
     # Enable DCONF
     programs.dconf.enable = true;
