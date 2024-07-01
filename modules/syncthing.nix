f1553975b81585803c60227414da12ee22a29ebb
4jamesccraven <4jamesccraven@gmail.com>
2024-06-30 20:09:51 -0400

    Add vaal to syncthing

--- modules/syncthing.nix
+++ modules/syncthing.nix
@@ -7,7 +7,7 @@
     overrideFolders = true;
     settings = {
       devices = {
-        # "vaal" =     { id = ""; };
+        "vaal" =     { id = "HVZXSAY-3P5YHIN-OO4L3PS-JSKEWV3-CS7MYTK-K5I5VAQ-7ISIYXW-PQ56HQN"; };
         "RioTinto" = { id = "6MRO7HT-VT4Y2WD-POMIEIW-52LCQKX-WDKRXPA-LYHEXFO-EADQ3EX-NIE4MAA"; };
         "tokoro" =   { id = "KAV5MFS-KB4WA37-IOLIUCT-46LU5UG-Z7NEXX4-QNHUP3I-6TZFN36-VYYPIAD"; };
       };
@@ -15,17 +15,17 @@
       folders = {
         "Documents" = {
           path = "~/Documents";
-          devices = [ "RioTinto" "tokoro" ];
+          devices = [ "RioTinto" "tokoro" "vaal" ];
         };
 
         "Code" = {
           path = "~/Code";
-          devices = [ "RioTinto" "tokoro" ];
+          devices = [ "RioTinto" "tokoro" "vaal" ];
         };
 
         "Pictures" = {
           path = "~/Pictures";
-          devices = [ "RioTinto" "tokoro" ];
+          devices = [ "RioTinto" "tokoro" "vaal" ];
         };
       };
 
