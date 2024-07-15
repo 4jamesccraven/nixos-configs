4a2b3c1fca5167b1f2a86e350b9fa00aaa847e0e
4jamesccraven <4jamesccraven@gmail.com>
2024-07-15 01:54:50 -0400

    mkdev fix

--- myPackages/mkdev/derivation.nix
+++ myPackages/mkdev/derivation.nix
@@ -3,12 +3,12 @@
 with python312Packages;
 buildPythonApplication {
   pname = "mkdev";
-  version = "2.0";
+  version = "2.0.1";
   src = pkgs.fetchFromGitHub {
     owner = "4jamesccraven";
     repo = "mkdev";
-    rev = "3ded78ac990e633da1fb14b7785cb68db3b5591e";
-    sha256 = "1l688m439r5q6nigwc35nsypl96in7bfvgcainzjw4zgc2di784i";
+    rev = "3253f017db1efa214d3b3d344d44b556d3c148fc";
+    sha256 = "195cnbzbksdps4la3hlqs2jj97aggkm1rjdhg6cabq5p4ds2z761";
   };
 
   buildInputs = [ python ];
