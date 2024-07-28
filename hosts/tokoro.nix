a7d75774cdd31f0e79968bd5f3154652d47dc057
4jamesccraven <4jamesccraven@gmail.com>
2024-07-28 17:32:36 -0400

    Fix tokoro hardware

--- hosts/tokoro.nix
+++ hosts/tokoro.nix
@@ -1,13 +1,14 @@
-{ configs, pkgs, ... }:
+{ config, pkgs, lib, inputs, modulesPath, ... }:
+
 
 {
   imports = [
-    <home-manager/nixos>
-    ../hardware-configuration.nix
+    inputs.home-manager.nixosModules.default
     ../modules/git.nix
     ../modules/gnome.nix
     ../modules/syncthing.nix
     ../myPackages
+    (modulesPath + "/installer/scan/not-detected.nix")
   ];
   
   system.stateVersion = "23.11";
@@ -95,4 +96,34 @@
 
     home.stateVersion = "24.05";
   }; 
+
+  ## Hardware transcluded
+
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ehci_pci" "ahci" "nvme" "usb_storage" "usbhid" "sd_mod" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-intel" ];
+  boot.extraModulePackages = [ ];
+
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/348657e2-5557-4ddc-a77f-3c22ac3e78f2";
+      fsType = "ext4";
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/E951-BC80";
+      fsType = "vfat";
+      options = [ "fmask=0022" "dmask=0022" ];
+    };
+
+  swapDevices = [ ];
+
+  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+  # (the default) this is the recommended approach. When using systemd-networkd it's
+  # still possible to use this option, but it's recommended to use it in conjunction
+  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+  networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp2s0.useDHCP = lib.mkDefault true;
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
