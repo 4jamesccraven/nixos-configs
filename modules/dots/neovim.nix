0689a80c85439312bd854bdb3a2cdc72c409f925
4jamesccraven <4jamesccraven@gmail.com>
2024-08-17 20:21:08 -0400

    minor change: make neotree work on start

--- modules/dots/neovim.nix
+++ modules/dots/neovim.nix
@@ -51,27 +51,39 @@
       '';
 
       extraLuaConfig = ''
+        -- Clipboard stuff (broken)
         vim.opt.clipboard = 'unnamedplus'
         vim.opt.mouse = 'a'
 
+        -- Tabs
         vim.opt.tabstop = 4
         vim.opt.softtabstop = 4
         vim.opt.shiftwidth = 4
         vim.opt.expandtab = true
 
+        -- Line numbers
         vim.opt.number = true
         vim.opt.relativenumber = true
 
+        -- Search config
         vim.opt.incsearch = true
         vim.opt.hlsearch = false
         vim.opt.ignorecase = true
         vim.opt.smartcase = true
 
+        -- Rebind plugin commands
         vim.api.nvim_create_user_command('FF', 'Telescope find_files', {})
         vim.cmd('cnoreabbrev ff FF')
-
         vim.api.nvim_create_user_command('NT', 'Neotree', {})
         vim.cmd('cnoreabbrev nt NT')
+
+        -- Open Neotree on launch
+        vim.api.nvim_create_autocmd("VimEnter", {
+          callback = function()
+            vim.cmd("Neotree")
+            vim.cmd("wincmd p")
+          end
+        })
       '';
     };
   };
