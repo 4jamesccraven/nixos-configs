2109e68d02417cd9e5d9ad5e6dd21ce90c0312b9
4jamesccraven <4jamesccraven@gmail.com>
2024-08-30 20:25:13 -0400

    minor change: added some generic devShells to the flake

--- flake.nix
+++ flake.nix
@@ -5,30 +5,60 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     home-manager.url = "github:nix-community/home-manager?ref=master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    # devShell stuff
+    python310-14.url = "github:nixos/nixpkgs/0cb2fd7c59fed0cd82ef858cbcbdb552b9a33465";
   };
 
   outputs = { nixpkgs, ... } @ inputs : {
-      nixosConfigurations = {
-        RioTinto = nixpkgs.lib.nixosSystem {
-          specialArgs = {inherit inputs; };
-          modules = [
-            ./hosts/RioTinto.nix
-          ];
-        };
-
-        vaal = nixpkgs.lib.nixosSystem {
-          specialArgs = {inherit inputs; };
-          modules = [
-            ./hosts/vaal.nix
-          ];
-        };
-
-        tokoro = nixpkgs.lib.nixosSystem {
-          specialArgs = {inherit inputs; };
-          modules = [
-            ./hosts/tokoro.nix
-          ];
-        };
+    nixosConfigurations = {
+      RioTinto = nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs; };
+        modules = [
+          ./hosts/RioTinto.nix
+        ];
       };
+
+      vaal = nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs; };
+        modules = [
+          ./hosts/vaal.nix
+        ];
+      };
+
+      tokoro = nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs; };
+        modules = [
+          ./hosts/tokoro.nix
+        ];
+      };
+    };
+
+    devShells.x86_64-linux =
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      dependencies = {
+        rust = with pkgs; [ cargo libgcc rustc ];
+        python310 = [ inputs.python310-14.legacyPackages.x86_64-linux.python310 ];
+      };
+    in {
+
+      rust = pkgs.mkShell {
+        buildInputs = dependencies.rust;
+
+        shellHook = ''
+          clear
+        '';
+      };
+
+      pct = pkgs.mkShell {
+        buildInputs = with dependencies; rust ++ python310;
+
+        shellHook = ''
+          clear
+        '';
+      };
+
+    };
   };
 }
