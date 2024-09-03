a90ff6eba79584c827ec6a2880c00c6e51e0e985
4jamesccraven <4jamesccraven@gmail.com>
2024-09-02 23:27:38 -0400

    minor fix: previous deletions were not pushed

--- myPackages/default.nix
+++
@@ -1,9 +0,0 @@
-{ ... }:
-
-{
-  # Custom Software override
-  nixpkgs.config.packageOverrides = pkgs: {
-    mkdev = pkgs.callPackage ./mkdev { };
-    dracula-cursors = pkgs.callPackage ./dracula-cursors { };
-  };
-}
