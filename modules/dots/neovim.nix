e716a0b77824c35a9f80160e27b470ff3da8c196
4jamesccraven <4jamesccraven@gmail.com>
2024-12-18 18:29:39 -0500

    fix: remove rust-analyzer stuttering

--- modules/dots/neovim.nix
+++ modules/dots/neovim.nix
@@ -84,6 +84,17 @@
 
           -- Set *.tex files to latex type
           vim.cmd([[autocmd BufRead,BufNewFile *.tex set filetype=latex]])
+
+          -- Fix for rust analyzer stuttering
+          for _, method in ipairs({ 'textDocument/diagnostic', 'workspace/diagnostic' }) do
+              local default_diagnostic_handler = vim.lsp.handlers[method]
+              vim.lsp.handlers[method] = function(err, result, context, config)
+                  if err ~= nil and err.code == -32802 then
+                      return
+                  end
+                  return default_diagnostic_handler(err, result, context, config)
+              end
+          end
         '';
 
       ### plugins ###
