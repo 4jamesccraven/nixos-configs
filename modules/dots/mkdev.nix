d28fd3d78f707dcf7fc61e96b327f5298241af6a
4jamesccraven <4jamesccraven@gmail.com>
2025-03-27 13:27:27 -0400

    fix: move mkdev pure config file to assets to better fit the config scheme

--- modules/dots/mkdev.nix
+++ modules/dots/mkdev.nix
@@ -3,7 +3,7 @@
 {
   home-manager.users.jamescraven = {
     home.file."/home/jamescraven/.config/mkdev/config.toml" = {
-      source = ./mkdev-config.toml;
+      source = ../../assets/mkdev-config.toml;
     };
   };
 }
