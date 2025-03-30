2857df17744a67873b220465c599db1ea6148ccd
4jamesccraven <4jamesccraven@gmail.com>
2025-03-30 15:17:26 -0400

    change: more declarative management of mkdev config

--- modules/dots/mkdev.nix
+++ modules/dots/mkdev.nix
@@ -1,9 +1,21 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
-  home-manager.users.jamescraven = {
-    home.file."/home/jamescraven/.config/mkdev/config.toml" = {
-      source = ../../assets/mkdev-config.toml;
-    };
+  home-manager.users.jamescraven =
+    let
+      format = pkgs.formats.toml {};
+      config = format.generate "config.toml" {
+        recipe_dir = "/home/jamescraven/nixos/assets/mkdev";
+
+        subs = {
+          day = "date +%d";
+          user = "whoami";
+          year = "date +%Y";
+          dir = "mk::dir";
+          month = "date +%m";
+        };
+      };
+    in {
+    xdg.configFile."mkdev/config.toml".source = config;
   };
 }
