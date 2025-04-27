fe1d27afb0157a252be75a140106bbfb60fee557
4jamesccraven <4jamesccraven@gmail.com>
2025-04-27 07:36:20 -0400

    Chore: system update

--- modules/dots/zsh.nix
+++ modules/dots/zsh.nix
@@ -16,31 +16,32 @@
         txt = "$HOME/Documents/Texts";
       };
 
-      initExtra = ''
-        # Disallow tabs at the start of prompt
-        zstyle ':completion:*' insert-tab false
+      initContent = # bash
+        ''
+          # Disallow tabs at the start of prompt
+          zstyle ':completion:*' insert-tab false
 
-        setopt auto_param_slash  # Dirs are autocompleted with a trailing /
-        setopt cdable_vars       # Try to prepend ~ if a cd command fails
-        setopt cd_silent         # Don't pwd after cd
-        setopt correct           # Offer to correct mispelled commands
+          setopt auto_param_slash  # Dirs are autocompleted with a trailing /
+          setopt cdable_vars       # Try to prepend ~ if a cd command fails
+          setopt cd_silent         # Don't pwd after cd
+          setopt correct           # Offer to correct mispelled commands
 
-        # fzf-zsh integration and theming
-        source <(${pkgs.fzf}/bin/fzf --zsh)
-        bindkey "^f" fzf-history-widget
+          # fzf-zsh integration and theming
+          source <(${pkgs.fzf}/bin/fzf --zsh)
+          bindkey "^f" fzf-history-widget
 
-        export FZF_DEFAULT_OPTS=" \
-        --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
-        --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
-        --color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
-        --color=selected-bg:#45475a \
-        --color=border:#313244,label:#cdd6f4"
+          export FZF_DEFAULT_OPTS=" \
+          --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
+          --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
+          --color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
+          --color=selected-bg:#45475a \
+          --color=border:#313244,label:#cdd6f4"
 
-        # PROMPT='%F{#CA9EE6}╭─(%f/ˈiː.ən/%F{#CA9EE6}@%m): [%f%~%F{#CA9EE6}]
-        # ╰─❯ %f'
+          # PROMPT='%F{#CA9EE6}╭─(%f/ˈiː.ən/%F{#CA9EE6}@%m): [%f%~%F{#CA9EE6}]
+          # ╰─❯ %f'
 
-        fastfetch
-      '';
+          fastfetch
+        '';
 
       shellAliases = {
         c = "clear";
