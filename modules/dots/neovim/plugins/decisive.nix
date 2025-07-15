83fb19b330774ad205c5f84d74d3af866f4fb3ac
4jamesccraven <4jamesccraven@gmail.com>
2025-07-15 01:15:43 -0400

    change: add md and csv plugins
    - From recent BreadOnPenguins video, inline md viewer and csv
      formatter/utility

---
+++ modules/dots/neovim/plugins/decisive.nix
@@ -0,0 +1,26 @@
+{ pkgs, ... }:
+
+let
+  decisive = pkgs.vimUtils.buildVimPlugin {
+    name = "decisive-nvim";
+    src = pkgs.fetchFromGitHub {
+      owner = "emmanueltouzery";
+      repo = "decisive.nvim";
+      rev = "c401541b8429b787d7dcb441e43bee63fc94737c";
+      hash = "sha256-uy+Nj+hfeei8nquZCzIxDYf1eQsaPMX26IMh/opOwG0=";
+    };
+  };
+in
+{
+  plugin = decisive;
+  type = "lua";
+  config = # lua
+    ''
+      --> decisive-nvim <--
+      -- Keybinds to enable the viewer
+      map('n', '<leader>cca', function() require'decisive'.align_csv({}) end)
+      map('n', '<leader>ccA', function() require'decisive'.align_csv_clear({}) end)
+      map('n', '[c', require'decisive'.align_csv_prev_col)
+      map('n', ']c', require'decisive'.align_csv_next_col)
+    '';
+}
