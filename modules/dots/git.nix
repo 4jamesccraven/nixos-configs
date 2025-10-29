5b4a4979aef16ba659b2262a2a717b29705e841f
4jamesccraven <4jamesccraven@gmail.com>
2025-10-29 08:35:21 -0400

    chore: system update

--- modules/dots/git.nix
+++ modules/dots/git.nix
@@ -3,34 +3,36 @@
 {
 
   home-manager.users.jamescraven = {
-    programs.git = {
-      enable = true;
-      userName = "4jamesccraven";
-      userEmail = "4jamesccraven@gmail.com";
-
-      includes =
-        let
-          catppuccin-delta = pkgs.fetchFromGitHub {
-            owner = "catppuccin";
-            repo = "delta";
-            rev = "e9e21cffd98787f1b59e6f6e42db599f9b8ab399";
-            hash = "sha256-04po0A7bVMsmYdJcKL6oL39RlMLij1lRKvWl5AUXJ7Q=";
+    programs = {
+      git = {
+        enable = true;
+        settings = {
+          init.defaultBranch = "main";
+          core.editor = "nvim";
+          user = {
+            name = "4jamesccraven";
+            email = "4jamesccraven@gmail.com";
           };
-        in
-        [
-          { path = "${catppuccin-delta}/catppuccin.gitconfig"; }
-        ];
+        };
+
+        includes =
+          let
+            catppuccin-delta = pkgs.fetchFromGitHub {
+              owner = "catppuccin";
+              repo = "delta";
+              rev = "e9e21cffd98787f1b59e6f6e42db599f9b8ab399";
+              hash = "sha256-04po0A7bVMsmYdJcKL6oL39RlMLij1lRKvWl5AUXJ7Q=";
+            };
+          in
+          [
+            { path = "${catppuccin-delta}/catppuccin.gitconfig"; }
+          ];
+      };
 
       delta = {
         enable = true;
-        options = {
-          features = "catppuccin-mocha";
-        };
-      };
-
-      extraConfig = {
-        init.defaultBranch = "main";
-        core.editor = "nvim";
+        enableGitIntegration = true;
+        options.features = "catppuccin-mocha";
       };
     };
   };
