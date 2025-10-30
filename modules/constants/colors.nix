e700cca2adca7ced6de766d4abf00e2dbe5f5d72
4jamesccraven <4jamesccraven@gmail.com>
2025-10-30 10:50:23 -0400

    change: refactor colour constants
    - Makes the colours depend only on manual declaration of the hex value,
      deriving other values where needed.
    - This is a revised, working version of the concept shown at
      Steven-S1020/Nixos-Configuration@56f30b4. Thanks Steve for the idea.

--- modules/constants/colors.nix
+++ modules/constants/colors.nix
@@ -17,6 +17,38 @@ let
       };
     };
   };
+
+  # String -> colorType
+  # Takes a hexadecimal colour string with or without the leading #
+  #
+  # Note that using TOML is necessary as a work around because nix has
+  # no hexadecimal direct parsing.
+  parseColor =
+    hex:
+    let
+      hexNoPrefix = removePrefix "#" hex;
+      channels = {
+        r = 1;
+        g = 2;
+        b = 3;
+      };
+
+      # Helper to extract the nth channel (n must be 1 indexed).
+      hexPairOf = n: builtins.substring ((n - 1) * 2) 2 hexNoPrefix;
+      # Helper that declares that some channel is the nth hex value.
+      # e.g., given hex := ffee11, defineAsTOML r 1 := "r = 0xff"
+      defineAsTOML = channel: index: "${channel} = 0x${hexPairOf index}";
+
+      # Create the TOML document, parse it, and coerce the values from ints to strings.
+      toml = lib.concatLines (lib.mapAttrsToList defineAsTOML channels);
+      rgbVals = builtins.fromTOML toml;
+      rgb = builtins.mapAttrs (_: builtins.toString) rgbVals;
+    in
+    {
+      hex = hexNoPrefix;
+      rgb = "${rgb.r}, ${rgb.g}, ${rgb.b}";
+      ansi = "38;2;${rgb.r};${rgb.g};${rgb.b}";
+    };
 in
 {
   options.colors = mkOption {
@@ -24,35 +56,15 @@ in
     description = "Named colour variables";
   };
 
-  config.colors = {
-    base = {
-      rgb = "30, 30, 46";
-      hex = "1e1e2e";
-      ansi = "38;2;30;30;46";
-    };
-
-    accent = {
-      rgb = "203, 166, 247";
-      hex = "cba6f7";
-      ansi = "38;2;203;166;247";
-    };
-
-    text = {
-      rgb = "205, 214, 244";
-      hex = "cdd6f4";
-      ansi = "38;2;205;214;244";
-    };
-
-    fail = {
-      rgb = "243, 139, 168";
-      hex = "f38ba8";
-      ansi = "38;2;243;139;168";
-    };
-
-    mantle = {
-      rgb = "24, 24, 37";
-      hex = "181825";
-      ansi = "38;2;24;24;37";
-    };
-  };
+  config.colors =
+    let
+      colours = {
+        base = "1e1e2e";
+        accent = "cba6f7";
+        text = "cdd6f4";
+        fail = "f38ba8";
+        mantle = "181825";
+      };
+    in
+    builtins.mapAttrs (_: parseColor) colours;
 }
