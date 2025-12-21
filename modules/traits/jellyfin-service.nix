f41cbdacfa93e3ac97a3ad2034ff9afa0d70afcd
4jamesccraven <4jamesccraven@gmail.com>
2025-12-21 09:01:32 -0500

    change: add nginx proxy for jellyfin

--- modules/traits/jellyfin-service.nix
+++ modules/traits/jellyfin-service.nix
@@ -7,10 +7,30 @@
   # Enable the service.
   services.jellyfin.enable = true;
   # Open the port to others
-  networking.firewall.allowedTCPPorts = [ 8096 ];
+  networking.firewall.allowedTCPPorts = [
+    8096
+    50924
+  ];
 
   # Having ffmpeg for debugging/editing is important.
   environment.systemPackages = with pkgs; [
     ffmpeg-full
   ];
+
+  # Set up reverse proxy to port forward jellyfin.
+  services.nginx = {
+    enable = true;
+    virtualHosts."_" = {
+      listen = [
+        {
+          addr = "0.0.0.0";
+          port = 50924;
+        }
+      ];
+
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:8096";
+      };
+    };
+  };
 }
