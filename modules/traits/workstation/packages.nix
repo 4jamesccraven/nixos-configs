661e6f8f4101d27b61167350b703c048e471a92d
4jamesccraven <4jamesccraven@gmail.com>
2026-03-10 13:04:49 -0400

    fix: formatting in `Workstation::packages`

--- modules/traits/workstation/packages.nix
+++ modules/traits/workstation/packages.nix
@@ -17,27 +17,35 @@
   environment.systemPackages = with pkgs; [
     # ---[ GUI ]---
     # :> Internet & Communication
+    # keep-sorted start
     brave
     telegram-desktop
     tor-browser
+    # keep-sorted end
 
     # :> Media
+    # keep-sorted start
     foliate
     makemkv
     obs-studio
     vlc
+    # keep-sorted end
 
     # :> Office
+    # keep-sorted start
     hunspell
     hunspellDicts.en_GB-large
     hunspellDicts.en_US-large
-    hunspellDicts.es_ES
     hunspellDicts.es_AR
+    hunspellDicts.es_ES
     libreoffice-qt
+    # keep-sorted end
 
     # :> Music
-    musescore
+    # keep-sorted start
     ardour
+    musescore
+    # keep-sorted end
 
     # :> Games
     (prismlauncher.override {
@@ -50,13 +58,16 @@
 
     # ---[ CLI ]---
     # :> Coreutils-esque
+    # keep-sorted start
     dust
     dysk
     fd
     just
     ripgrep
+    # keep-sorted end
 
     # :> Other Utilities
+    # keep-sorted start
     caligula
     ffmpeg-full
     file
@@ -68,41 +79,52 @@
     yazi
     yt-dlp
     zip
+    # keep-sorted end
 
     # :> Fun
     figlet
 
     # :> Scripts
-    update-notify
+    # keep-sorted start
     ns
+    update-notify
+    # keep-sorted end
 
     # ---[ Development ]---
     # :> Compilers
+    # keep-sorted start
     libgcc
     rustc
+    # keep-sorted end
 
     # :> Interpreters
     python314
 
     # :> Writing & Presenting
+    # keep-sorted start
     typst
     typstyle
+    # keep-sorted end
 
     # :> Project Management
+    # keep-sorted start
     cargo
     gh
     git
+    # keep-sorted end
   ];
 
   # ---[ Progam/Service-based ]---
-  programs.nh.enable = true;
-  programs.zsh.enable = true;
-  services.blueman.enable = true;
-  programs.direnv = {
-    enable = true;
-    loadInNixShell = true;
-    nix-direnv.enable = true;
+  programs = {
+    nh.enable = true;
+    zsh.enable = true;
+    direnv = {
+      enable = true;
+      loadInNixShell = true;
+      nix-direnv.enable = true;
+    };
   };
+  services.blueman.enable = true;
 
   # ---[ Fonts ]---
   fonts = {
