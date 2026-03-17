17ab26467f267c34fb946c114f3c827635560ad2
4jamesccraven <4jamesccraven@gmail.com>
2026-03-17 13:27:36 -0400

    fix: argument order of overlay in libFlake

--- lib/libFlake.nix
+++ lib/libFlake.nix
@@ -86,6 +86,6 @@ in
     let
       mkPkg = prev: name: prev.callPackage (dir + "/${name}.nix") { };
     in
-    _final: prev: genFileAttrs dir (mkPkg prev);
+    prev: _final: genFileAttrs dir (mkPkg prev);
 
 }
