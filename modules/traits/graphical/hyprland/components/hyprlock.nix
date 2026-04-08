48e81721cd9cfc99b9f46350ba55e97b5113432e
4jamesccraven <4jamesccraven@gmail.com>
2026-04-08 05:45:07 -0400

    change: change hyprlock style to be darker

--- modules/traits/graphical/hyprland/components/hyprlock.nix
+++ modules/traits/graphical/hyprland/components/hyprlock.nix
@@ -6,93 +6,83 @@
   Config for hyprlock, a lock screen for hyprland.
 */
 let
-  inherit (config.ext) flakeRoot;
-  wpPath = flakeRoot + /assets/wp-wide.png;
-  logoPath = flakeRoot + /assets/nixos-logo.png;
+  inherit (config.ext) colours;
+  funcFmt = colour: "rgb(${colour.rgb})";
+
+  colourVars = lib.genAttrs [
+    "base"
+    "accent"
+    "text"
+    "fail"
+    "crust"
+  ] (name: funcFmt colours."${name}");
+
+  font_family = "Overpass";
 in
+with colourVars;
 {
   config = lib.mkIf config.hyprland.enable {
-    home-manager.users.jamescraven =
-      let
-        inherit (config.ext) colours;
-        base = "rgb(${colours.base.rgb})";
-        accent = "rgb(${colours.accent.rgb})";
-        text = "rgb(${colours.text.rgb})";
-        fail = "rgb(${colours.fail.rgb})";
-      in
-      {
-        programs.hyprlock = {
-          enable = true;
+    home-manager.users.jamescraven = {
+      programs.hyprlock = {
+        enable = true;
 
-          settings = {
-            # :> General
-            background = {
+        settings = {
+          # :> General
+          background = {
+            monitor = "";
+            color = base;
+
+            blur_passes = 2;
+          };
+
+          # ---[ Contents ]---
+          # :> Time & Date
+          label = [
+            {
               monitor = "";
-              path = "${wpPath}";
-
-              blur_passes = 2;
-            };
-
-            # ---[ Contents ]---
-            # :> Time & Date
-            label = [
-              {
-                monitor = "";
-                text = "$TIME";
-                font_size = 95;
-                font_family = "FiraCode Nerd Font Mono";
-
-                halign = "center";
-                valign = "center";
-                position = "0, 10%";
-
-                color = text;
-              }
-
-              {
-                monitor = "";
-                text = "cmd[update:1000] echo $(date '+%a %B %d')";
-                font_size = 25;
-                font_family = "FiraCode Nerd Font Mono";
-
-                halign = "center";
-                valign = "center";
-                position = "0, 2%";
-
-                color = text;
-              }
-            ];
-
-            # :> NixOS logo
-            image = {
-              monitor = "";
-              path = "${logoPath}";
-              size = 80;
-
-              halign = "center";
-              valign = "bottom";
-
-              border_color = accent;
-            };
-
-            # :> Password Box
-            input-field = {
-              monitor = "";
-              size = "10%, 4%";
-              fade_on_empty = false;
+              text = "$TIME";
+              font_size = 95;
+              inherit font_family;
 
               halign = "center";
               valign = "center";
-              position = "0, -10%";
+              position = "0, 10%";
 
-              outer_color = accent;
-              inner_color = base;
-              font_color = text;
-              check_color = text;
-              fail_color = fail;
-            };
+              color = accent;
+            }
+
+            {
+              monitor = "";
+              text = "cmd[update:1000] echo $(date '+%a %B %d')";
+              font_size = 25;
+              inherit font_family;
+
+              halign = "center";
+              valign = "center";
+              position = "0, 2%";
+
+              color = text;
+            }
+          ];
+
+          # :> Password Box
+          input-field = {
+            monitor = "";
+            size = "10%, 4%";
+            fade_on_empty = false;
+
+            halign = "center";
+            valign = "center";
+            position = "0, -10%";
+
+            outer_color = accent;
+            inner_color = base;
+            font_color = text;
+            check_color = text;
+            fail_color = fail;
           };
         };
       };
+    };
   };
 }
