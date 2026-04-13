b578de6566ae250571607289d01025cca92a90da
4jamesccraven <4jamesccraven@gmail.com>
2026-04-13 10:02:18 -0400

    fix: cleanup treesitter config

--- modules/dots/neovim/lua/plugins/treesitter.lua
+++ modules/dots/neovim/lua/plugins/treesitter.lua
@@ -4,22 +4,15 @@ return function()
     vim.api.nvim_create_autocmd("FileType", {
         pattern = "*",
         callback = function(event)
+            -- manually exclude by filetype
             local bufnr = event.buf
             local ft = vim.bo[bufnr].filetype
             local excluded = {
                 csv = true, -- Decisive.nvim provides a better parser
             }
-            -- -- no parser for ft or explicitly excluded
-            if excluded[ft] then
-                return
-            end
+            if excluded[ft] then return end
 
             pcall(vim.treesitter.start)
-            --
-            -- local ok = pcall(vim.treesitter.get_parser, bufnr, ft)
-            -- if ok then
-            --     vim.treesitter.start(bufnr, ft)
-            -- end
         end,
     })
 end
