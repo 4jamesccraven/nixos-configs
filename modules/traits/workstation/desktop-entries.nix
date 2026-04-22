09e90802ff5a30a695c4867b2cadfcce304b29c7
4jamesccraven <4jamesccraven@gmail.com>
2026-04-22 01:10:48 -0400

    change: add desktop entries for cams

--- modules/traits/workstation/desktop-entries.nix
+++ modules/traits/workstation/desktop-entries.nix
@@ -1,14 +1,40 @@
-{ ... }:
+{ pkgs, lib, ... }:
 
 /*
   ====[ XDG Desktop Entries ]====
   :: in trait `Workstation`
   A set of XDG Desktop entries to be used with application launchers (e.g., Fuzzel).
 */
-{
-  home-manager.users.jamescraven = {
-    xdg.desktopEntries = {
-      # I'm leaving this stub for now, but I don't have any custom desktop entries atm.
+let
+  vlc = lib.getExe pkgs.vlc;
+
+  mkEntry =
+    {
+      n,
+      name,
+      url ? null,
+    }:
+    let
+      camNumber = lib.toString n;
+      stream = if url != null then url else "rtsp://cams:855${camNumber}/cam${camNumber}";
+    in
+    pkgs.makeDesktopItem {
+      name = "cam${camNumber}";
+      desktopName = "cam${camNumber} (${name})";
+      exec = "${vlc} --no-qt-error-dialogs --qt-minimal-view \"--key-play-pause=\" \"--key-pause=\" ${stream}";
+      icon = "accessories-camera";
+      categories = [ "Network" ];
     };
-  };
+
+  camNames = [
+    "Driveway"
+    "Backyard"
+  ];
+  systemPackages = lib.pipe camNames [
+    (lib.imap1 (n: name: { inherit n name; }))
+    (map mkEntry)
+  ];
+in
+{
+  environment = { inherit systemPackages; };
 }
